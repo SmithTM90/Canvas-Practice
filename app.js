5d5703ad9f44a9490046babb609bffcb5c34745a
Travis Smith <smithtm90@gmail.com>
2016-08-24 16:08:55 -0700

    not working yet

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var canvasDiv = document.getElementById('canvasDiv');
 var canvasWidth = 400;
 var canvasHeight = 200;
+var colorPurple = "#cb3594";
+var colorGreen = "#659b41";
+var colorYellow = "#ffcf33";
+var colorBrown = "#986928";
+var clickSize = new Array();
+var curSize = "normal";
+var clickTool = new Array();
+var curTool = "crayon";
+
+var curColor = colorPurple;
+var clickColor = new Array();
+
 canvas = document.createElement('canvas');
 canvas.setAttribute('width', canvasWidth);
 canvas.setAttribute('height', canvasHeight);
@@ -39,28 +51,48 @@ var clickY = newArray();
 var clickDrag = newArray();
 var paint;
 
-function addClick(x,y,dragging) {
+function addClick(x, y, dragging)
+{
   clickX.push(x);
   clickY.push(y);
   clickDrag.push(dragging);
+  if(curTool == "eraser"){
+    clickColor.push("white");
+  }else{
+    clickColor.push(curColor);
+  }
+  clickColor.push(curColor);
+  clickSize.push(curSize);
 }
 
-function redraw() {
-  context.clearRect(0,0,context.canvas.width, context.canvas.height);
-
-  context.strokeStyle = "#192";
+function redraw(){
+  /* context.strokeStyle = "#df4b26"; */
   context.lineJoin = "round";
   context.lineWidth = 5;
 
-  for(vari=0; i < clickX.length; i++) {
+  context.save();
+  context.beginPath();
+  context.rect(drawingAreaX, drawingAreaY, drawingAreaWidth, drawingAreaHeight);
+  context.clip();
+      
+  for(var i=0; i < clickX.length; i++){   
     context.beginPath();
-    if(clickDrag[i] && i) {
-      context.moveto(clickX[i-1], clickY[i-1]);
-    } else {
-      context.moveto(clickX[i]-1, clickY[i])
+    if(clickDrag[i] && i){
+      contex.moveTo(clickX[i-1], clickY[i-1]);
+    }else{
+      context.moveTo(clickX[i]-1, clickY[i]);
     }
-    context.lineto(clickX[i], clickY[i]);
+    context.restore();
+    context.lineTo(clickX[i], clickY[i]);
     context.closePath();
+    context.strokeStyle = clickColor[i];
+    context.lineWidth = radius;
     context.stroke();
+
+    if(curTool == "crayon") {
+    context.globalAlpha = 0.4;
+    context.drawImage(crayonTextureImage, 0, 0, canvasWidth, canvasHeight);
+  }
+  context.globalAlpha = 1;
   }
 }
